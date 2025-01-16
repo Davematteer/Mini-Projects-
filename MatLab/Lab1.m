a63630148823f309c4b320c503fbf80faa21dc67
Davematteer <davematteer@gmail.com>
2025-01-16 14:47:33 +0000

    Processing card data

---
+++ MatLab/Lab1.m
@@ -0,0 +1,21 @@
+% Lab1 
+
+%% Question 1
+% from 0 to 30 
+rand * 30;
+
+% from 10 to 100
+10+ (rand * 90);
+
+% from 1 to 20
+randi([1,20]);
+
+% from 30 to 80
+randi([30,80]);
+
+
+%% Question 2
+
+radius = 10;
+area = pi * (radius^2);
+area
