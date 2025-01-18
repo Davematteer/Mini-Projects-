39fe45a7724ab7f1d0c07f8ffce465b665f4c8ac
Davematteer <davematteer@gmail.com>
2025-01-18 00:02:09 +0000

    Front end work

--- YugiohProject/front_end/lib/pages/IntroPage.dart
+++ YugiohProject/front_end/lib/pages/IntroPage.dart
@@ -2,11 +2,25 @@ import 'package:flutter/material.dart';
 
 
 class IntroPage extends StatelessWidget{
+
+  const IntroPage({super.key});
   
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
+    return Scaffold(
+      body: Column(
+        children: [
+          //logo 
+            Padding(
+              padding: const EdgeInsets.all(80.0),
+              child: Image.asset('lib/images/planting.png'),
+            )
+          // text introducing
 
+
+          // get started button
+        ],
+      )
     );
   }
 }
