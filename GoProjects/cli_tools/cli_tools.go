6f419fc9c9b89329906031baf32f3efa63e7d217
Davematteer <davematteer@gmail.com>
2025-01-21 16:35:16 +0000

    Day 2 of learning go

--- GoProjects/cli_tools/cli_tools.go
+++ GoProjects/cli_tools/cli_tools.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func main() {
-	sum := 0
-
-	for sum < 5 {
-		sum += 1
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
 	}
+	return fmt.Sprint(math.Sqrt(x))
+}
 
-	fmt.Println(sum)
+func main() {
+	fmt.Println(sqrt(2), sqrt(-4))
 }
