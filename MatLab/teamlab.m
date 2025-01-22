a04a90aa98ff71ce0666241f04fba13aed803c33
Davematteer <davematteer@gmail.com>
2025-01-22 22:21:06 +0000

    More of this matlab bs

---
+++ MatLab/teamlab.m
@@ -0,0 +1,13 @@
+%% Question 1
+%1 No error
+%2 No closing bracket and incomplete terms 
+%3 "]" missing and incompatible dimensions
+%4 None 
+%5 Element no present or exceeding array bound 
+%6 Incompatible sizes 
+%7 None 
+%8 None 
+%9 No element-wise addition present in Matlab
+%10 None 
+%11 Incompatible dimensions 
+%12 
