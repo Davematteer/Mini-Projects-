307a8327999126944c1e96926eb78cd115677d66
Davematteer <davematteer@gmail.com>
2025-01-26 17:16:06 +0000

    making httpserver

--- GoProjects2/fetchData.go
+++ GoProjects2/fetchData.go
@@ -1,23 +1,28 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"os"
-	"time"
-	"errors"
 )
 
 const serverPort = 3333
 
-func main(){
+func main() {
 	go func() {
 		mux := http.NewServeMux()
-		mux.HandleFunc("/",func (w http.ResponseWriter, r *http.Request)  {
-			fmt.Println("server: %s /\n",r.Method)	
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println("server: %s /\n", r.Method)
 		})
 		server := http.Server{
-			Addr: fmt.
+			Addr:    fmt.Sprintf(":%d", serverPort),
+			Handler: mux,
 		}
-	}
+		if err := server.ListenAndServe(); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				fmt.Printf("error running http server: %s\n", err)
+			}
+		}
+	}()
+
 }
