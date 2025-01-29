d36721ed400b1f0badb29172986368943f2b95aa
Davematteer <davematteer@gmail.com>
2025-01-29 13:39:16 +0000

    workign on yugiohapp

--- yugioh/fetchData.go
+++ yugioh/fetchData.go
@@ -5,12 +5,31 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	api_key := ""
-	parameters := ""
-	endpoint = fmt.Sprintf("")
+
+	fetchData("Dark Magician")
+
+}
+
+type Card struct {
+	Data []struct {
+		Name     string   `json:"name"`
+		Typeline []string `json:"typeline"`
+		Cardtype string   `json:"type"`
+		Attack   int      `json:"atk"`
+		Defence  int      `json:"def"`
+	} `json:"data"`
+}
+
+func fetchData(cardName string) {
+
+	baseUrl := "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+	parameter := url.Values{}
+	parameter.Add("name", cardName)
+	endpoint := fmt.Sprintf("%s?%s", baseUrl, parameter.Encode())
 
 	res, err := http.Get(endpoint)
 
@@ -25,7 +44,5 @@ func main() {
 	}
 
 	bodystring := string(body)
-
 	fmt.Println(bodystring)
-
 }
