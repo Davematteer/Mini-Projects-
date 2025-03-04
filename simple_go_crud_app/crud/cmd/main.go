368a814c46aace622bf074fef9dfa91da7965c89
Davematteer <davematteer@gmail.com>
2025-03-04 23:44:10 +0000

    a simple crud api in golang

---
+++ simple_go_crud_app/crud/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/Davematteer/simple_go_crud_app/crud/pkg/handlers"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/books", handlers.GetAllBooks).Methods(http.MethodGet)
+
+	log.Println("API is running")
+	http.ListenAndServe(":4000", router)
+}
---
+++ simple_go_crud_app/crud/pkg/mocks/book.go
@@ -0,0 +1,12 @@
+package mocks
+
+import "github.com/Davematter/simple_go_crud_app/crud/pkg/models"
+
+var Books = []models.Books{
+	{
+		ID:     1,
+		Title:  "Golang",
+		Author: "Daveyboi",
+		Desc:   "A book on golang",
+	},
+}
