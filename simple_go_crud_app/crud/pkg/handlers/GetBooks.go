d08797dc2d8f132a594cd375f435661864a2ef31
Davematteer <davematteer@gmail.com>
2025-03-06 04:35:17 +0000

    a simple crud api in golang

--- simple_go_crud_app/crud/pkg/handlers/GetBooks.go
+++ simple_go_crud_app/crud/pkg/handlers/GetBooks.go
@@ -10,18 +10,20 @@ import (
 	"github.com/Davematteer/simple_go_crud_app/pkg/mocks"
 )
 
-func DeleteBook(w http.ResponseWriter, r *http.Request) {
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	// Read dynamic id parameter
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
 
-	// Iterate over the books
-	for index, book := range mocks.Books {
+	// Iterate over all the mock books
+	for _, book := range mocks.Books {
 		if book.ID == id {
-			mocks.Books = append(mocks.Books[:index], mocks.Books[index+1:]...)
+			// If ids are equal send book as a response
 
 			w.Header().Add("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode("Deleted")
+
+			json.NewEncoder(w).Encode(book)
 			break
 		}
 	}
--- simple_go_crud_app/crud/pkg/handlers/UpdateBook.go
+++ simple_go_crud_app/crud/pkg/handlers/UpdateBook.go
@@ -1,1 +1,48 @@
 package handlers
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+
+	"github.com/Davematteer/simple_go_crud_app/pkg/mocks"
+	"github.com/Davematteer/simple_go_crud_app/pkg/models"
+)
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	// Read dynamic id parameter
+	vars := mux.Vars(r)
+	id, _ := strconv.Atoi(vars["id"])
+
+	// Read request body
+	defer r.Body.Close()
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var updatedBook models.Book
+
+	json.Unmarshal(body, &updatedBook)
+
+	// Iterate over all the mock books
+	for index, book := range mocks.Books {
+		if book.ID == id {
+
+			book.Title = updatedBook.Title
+			book.Author = updatedBook.Author
+			book.Desc = updatedBook.Desc
+
+			mocks.Books[index] = book
+
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode("Updated")
+			break
+		}
+	}
+}
